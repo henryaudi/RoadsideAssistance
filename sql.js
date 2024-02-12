22e5c9c79fd56a3eb7d95f38ce7f6362fed9a031
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-02-12 11:13:00 -0600

    new file:   .gitignore 	modified:   .idea/Final Project.iml 	new file:   .idea/dataSources.local.xml 	new file:   .idea/dataSources.xml 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da.xml 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da/storage_v2/_src_/schema/ebean_prisma.KldSoQ.meta 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da/storage_v2/_src_/schema/information_schema.FNRwLQ.meta 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da/storage_v2/_src_/schema/mysql.osA4Bg.meta 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da/storage_v2/_src_/schema/performance_schema.kIw0nw.meta 	new file:   .idea/dataSources/89539ac0-567d-4045-b078-c6b4e18798da/storage_v2/_src_/schema/sys.zb4BAA.meta 	modified:   bin/www 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/images/FilingReport.png 	modified:   sql.js

--- sql.js
+++ sql.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'xxxxx',
-    database:'finalproject'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-db.connect();
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database successfully!');
+});
 
 module.exports = db;
